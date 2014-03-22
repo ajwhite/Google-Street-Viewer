9146c0896633237c72401cf23b792507d5425c7c
Atticus White <contact@atticuswhite.com>
2014-03-21 22:07:46 -0400

    updates

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 		</div>
 	</div>
 	
+	<div class="row loading-section">
+		<div class="col-md-12">
+			<h3>Loading paths...</h3>
+			<div class="progress">
+				<div class="progress-bar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+			</div>
+			
+		</div>
+	</div>
+	
 	<div class="row">
 		<div class="col-md-12">
 			<div id="map"></div>
@@ -14,7 +24,6 @@
 	</div>
 </div>
 
-<a href="#" id="move">Move</a>
 <style type="text/css">
 #map { width: 800px; height: 500px; background-color:gray;}
 </style>
@@ -22,64 +31,10 @@
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAU0mkZ8G0JLpXpbCqX4-vIJC0UZcFEFKU&sensor=false"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script type="text/javascript" src="streetviewer.js"></script>
-<script type="text/javascript" src="GSVPano.js"></script>
 
 
 <script type="text/javascript">
 
-$(window).load(function(){
-	return;
-	var directionService = new google.maps.DirectionsService();
-	
-	var request = {
-		origin: 'Boston, MA',
-		destination: 'Waltham, MA',
-		travelMode: google.maps.TravelMode.DRIVING
-	};
-	
-	directionService.route(request, function(response, status){
-		if (status == google.maps.DirectionsStatus.OK){
-			console.log("directions", response);
-		}
-		
-	});
-
-
-	return;
-
-	var point = new google.maps.LatLng(42.345573,-71.098326);
-	var loader = new GSVPANO.PanoLoader({ zoom: 1 });
-	
-	loader.load(point, function(){
-		console.log('initailized');
-	});
-		loader.composePanorama("gq21nsoEPpzTNpIlQWc5Nw");
-	
-	/* gq21nsoEPpzTNpIlQWc5Nw */
-	loader.onPanoramaLoad = function(){
-		console.log("onPanoramaLoad", loader.canvas);
-		
-		$("#map").append(loader.canvas);
-		/*
-		var img = new Image();
-		img.id = 'test';
-		img.src = loader.canvas.toDataURL();
-		$("#map").append(img);
-		*/
-		
-		//context.drawImage(loader.canvas, 0, 0);
-	};
-	
-/*	
-	var canvas = document.createElement("canvas");
-	var context = canvas.getContext('2d');
-	canvas.setAttribute('width',this.canvas.width);
-	canvas.setAttribute('height',this.canvas.height);
-	context.drawImage(this.canvas, 0, 0);
-*/
-
-
-});
 
 </script>
 			
--- streetviewer.js
+++ streetviewer.js
@@ -1,6 +1,36 @@
 (function($){
 	"use strict";
 	
+	
+	function degree2Radian(degree){
+		return ((degree * Math.PI) / 180.0);
+	}
+	
+	function Point2PointBearing(pt1, pt2) {
+	    var angle = 0;
+	    if (pt1 != null && pt2 != null && !pt1.equals(pt2)) {
+	        var y1 = degree2Radian(pt1.lat());
+	        var x1 = degree2Radian(pt1.lng());
+	        var y2 = degree2Radian(pt2.lat());
+	        var x2 = degree2Radian(pt2.lng());
+	        var a = Math.sin(x1 - x2) * Math.cos(y2);
+	        var b = Math.cos(y1) * Math.sin(y2) - Math.sin(y1) * Math.cos(y2) * Math.cos(x1 - x2);
+	        angle = -(Math.atan2(a, b));
+	        if (angle < 0.0) {
+	            angle += Math.PI * 2.0;
+	        }
+	        angle = parseInt(angle * 180.0 / Math.PI);
+	    }
+	    return angle;
+	}
+	
+	function Point(latlng){
+		this.latlng = null;
+		this.bearing = null;
+		this.pano = null;
+	}
+	
+
 	function StreetViewer(){
 		this.mapID = "map";
 		this.map = null;
@@ -8,87 +38,150 @@
 		this.streetViewService = null;
 		this.directionsService = null;
 		
-		
+		this.maxPoints = 100;
+		this.points = [];
+		this.path = [];
+		this.currentStep = 0;
 		this.pitch = 10;
 	};
 	
+	
 	StreetViewer.prototype.init = function(){
 		this.streetViewService = new google.maps.StreetViewService();
-		this.directionsService = new google.maps.DirectionsService();
-		
-		
-		//this.map = new google.maps.Map(document.getElementById(this.mapID), mapOptions);
-		//this.streetView = this.map.getStreetView();
-		//this.streetView.setOptions(streetViewOptions);		
+		this.directionsService = new google.maps.DirectionsService();	
 	};
 	
-	StreetViewer.prototype.handleDirections = function( direction ){
+	StreetViewer.prototype.mapInit = function(){
+		var mapOptions = {
+			center: this.path[0],
+			zoom: 12
+		};
+		this.map = new google.maps.Map(document.getElementById(this.mapID), mapOptions);
+		this.streetView = this.map.getStreetView();
+		this.streetView.setOptions({
+			position: mapOptions.center, 
+			pov: {
+				heading: 100,
+				pitch: 10
+			},
+			visible: true
+		});
+	}
+	
+	StreetViewer.prototype.traverse = function(){
+		var self = this,
+			point = this.points[this.currentStep];
+		
+		console.log("POINT", point);
+		this.streetView.setPano(point.pano);
+		this.streetView.setPov({
+			heading: point.bearing,
+			pitch: 10
+		});
+		//this.map.setCenter(this.path[this.currentStep]);
+		this.currentStep++;
+		
+		setTimeout(function(){
+			self.traverse();
+		},800);
+		
+		if (this.currentStep > this.maxPoints) return;
+	}
+	
+	StreetViewer.prototype.handleDirections = function( directions ){
 		console.log("Direcitons", directions);
+		var routes = directions.routes;
+			//path = routes[0].overview_path;
+			
+		
+		var count = 0;
+		var steps = routes[0].legs[0].steps;
+		for (var i=0; i<steps.length; i++){
+			for (var j=0; j<steps[i].lat_lngs.length; j++){
+				this.path.push(steps[i].lat_lngs[j]);
+			}
+		}
+		this.mapInit();
+		this.buildPoints();
+	};
+	
+	function updateProgress(step, total){
+		var percent = (step / total) * 100;
+		if (percent >= 100){
+			setTimeout(function(){
+				$(".loading-section").fadeOut();
+			}, 1000);
+		} else {
+			$(".progress-bar").css('width', percent + '%');
+		}
+	}
+	
+	
+	StreetViewer.prototype.buildPoints = function(){
+		var self = this,
+			i = 0,
+			j = 0;
+		function getPano(){
+			self.streetViewService.getPanoramaByLocation(self.path[i], 50, function(result, status){
+				if (j>self.maxPoints || i == self.path.length-1) {
+					self.buildPointsComplete();
+					return;
+				}
+				if (status == google.maps.StreetViewStatus.OK){
+					
+					var point = new Point();
+					point.latlng = self.path[i];
+					point.bearing = Point2PointBearing(self.path[i], self.path[i+1]);
+					point.pano = result.location.pano;
+					self.points.push(point);
+					j++;
+					
+					updateProgress(j, self.maxPoints);
+				}
+				i++;
+				getPano();
+			});
+		};
+		
+		getPano();
+		console.log("Building points..");
+		/*
+		 
+		
+		this.streetViewService.getPanoramaByLocation()
+		for(var i=0; i<this.path.length-1; i++){
+			var point = new Point();
+			point.bearing = Point2PointBearing(this.path[i], this.path[i+1]);
+			point.latlng = this.path[i];
+			
+		}
+		*/
+	};
+	
+	StreetViewer.prototype.buildPointsComplete = function(){
+		console.log("Build complete");
+		console.log("Points", this.points);	
+		this.traverse();
 	};
 	
 	StreetViewer.prototype.getDirections = function(origin, destination){
+		var self = this;
+		origin = "West Townsend, MA";
+		destination = "Toronto, ON";
 		var request = {
 			origin: origin,
 			destination: destination,
 			travelMode: google.maps.TravelMode.DRIVING
 		}
 	
-		this.directionService.route(request, function(response, status){
+		this.directionsService.route(request, function(response, status){
 			if (status == google.maps.DirectionsStatus.OK){
-				self.handleDirections(repsonse);
+				self.handleDirections(response);
 			}
 			
 		});
 	};
-	
-	StreetViewer.prototype.orient = function(){
-		var links = this.streetView.getLinks();
-		this.streetView.setPov({
-			heading: links[0].heading,
-			pitch: this.pitch
-		});
-	};
-	
-	
-	StreetViewer.prototype.move = function(){
-		var target = this.getNextLink();
 		
-		this.streetView.setPano(target.pano);
-		this.streetView.setPov({
-			heading: target.heading,
-			pitch: this.pitch
-		});
-	};
-	
-	
-	StreetViewer.prototype.getNextLink = function(){
-		var links = this.streetView.getLinks(),
-			currentHeading = this.streetView.getPov().heading,
-			closestHeading = false;
-		
-		var delta = false;
-		console.log("Current heading", currentHeading);
-		for (var i=0; i<links.length; i++){
-			console.log("candidate heading", links[i].heading);
-			
-			var d = Math.abs(currentHeading - links[i].heading);
-			if (delta === false){
-				delta = d;
-				closestHeading = i;
-			} else if (d < delta) {
-				delta = d;
-				closestHeading = i;
-			}
-		}
-		
-		console.log("Selected heading", links[closestHeading].heading);
-		
-		return links[closestHeading];
-		
-	};
-	
-	
-	
 	
 	$(window).load(function(){
 		var streetViewer = new StreetViewer();
